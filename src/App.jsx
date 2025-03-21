588451b01d0284310d2c20a51b1e60b1830be0fd
ximbiosis <javier.alejandro.canto@gmail.com>
2025-03-21 11:47:07 -0300

    Commit de prueba a Vercel

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function App() {
+  return (
+      <div>
+        <div>Hola Mundo !</div>
+      </div>
+  )
+}
+
+export default App
---
+++ src/main.jsx
@@ -0,0 +1,6 @@
+import React, { createRoot } from 'react-dom/client'
+import App from './App.jsx'
+
+createRoot(document.getElementById('root')).render(
+    <App />
+)
